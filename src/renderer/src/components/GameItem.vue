7a0a81e907b7578956f9e6277204e50fe86cecda
Jonn Hard <hardjonn@gmail.com>
2025-05-19 23:14:18 -0400

    add delete button

--- src/renderer/src/components/GameItem.vue
+++ src/renderer/src/components/GameItem.vue
@@ -10,6 +10,7 @@ const processingActions = {
   uploadingGameToRemote: 'Uploading game to remote...',
   abortingGameUpload: 'Aborting game upload...',
   refreshingHashAndSize: 'Refreshing hash and size...',
+  initGameDeletion: 'Initiating game deletion...',
 };
 
 const props = defineProps({
@@ -43,6 +44,8 @@ const data = reactive({
 
   progressContent: null,
   showProgressContent: false,
+
+  deletePrefix: false,
 
   gameItem: { ...props.gameItem },
   progress: props.progress,
@@ -244,6 +247,46 @@ const shouldShowRefreshHashAndSizeButton = computed(() => {
   }
 
   if (isProcessingAction(processingActions.uploadingGameToRemote)) {
+    return false;
+  }
+
+  return true;
+});
+
+const shouldShowDeleteGameFromLocalButton = computed(() => {
+  if (data.gameItem.source === 'steam') {
+    return false;
+  }
+
+  if (data.gameItem.status !== 'ACTIVE') {
+    return false;
+  }
+
+  if (isProcessingAction(processingActions.editingGameItem)) {
+    return false;
+  }
+
+  if (isProcessingAction(processingActions.savingGameItem)) {
+    return false;
+  }
+
+  if (isProcessingAction(processingActions.uploadingGameToRemote)) {
+    return false;
+  }
+
+  if (isProcessingAction(processingActions.refreshingHashAndSize)) {
+    return false;
+  }
+
+  if (isProcessingAction(processingActions.initGameDeletion)) {
+    return false;
+  }
+
+  if (!data.gameItem.realLocalGamePath) {
+    return false;
+  }
+
+  if (data.gameItem.localState.isDownloading) {
     return false;
   }
 
@@ -454,6 +497,18 @@ async function onActionRefreshHashAndSize() {
   }
 }
 
+async function onActionInitGameDeletion() {
+  activateProcessingAction(processingActions.initGameDeletion);
+}
+
+async function onActionCancelGameDeletion() {
+  data.processingAction = null;
+}
+
+async function onActionConfirmGameDeletion() {
+  console.log('Confirming game deletion with prefix deletion:', data.deletePrefix);
+}
+
 function makeRawGameItem() {
   const rawGameItem = toRaw(data.gameItem);
 
@@ -596,10 +651,19 @@ function makeRawGameItem() {
         <button
           v-if="isProcessingAction(processingActions.editingGameItem)"
           type="button"
-          class="text-white w-full bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm py-2.5 text-center mb-2"
+          class="text-white w-full bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br dark:focus:ring-red-800 shadow-lg dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm py-2.5 text-center mb-2"
           @click="onActionEditCancel"
         >
           Cancel and Reset
+        </button>
+
+        <button
+          v-if="shouldShowDeleteGameFromLocalButton"
+          type="button"
+          class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br dark:focus:ring-red-800 shadow-lg dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2"
+          @click="onActionInitGameDeletion"
+        >
+          Delete Game From Local
         </button>
       </div>
     </div>
@@ -608,6 +672,57 @@ function makeRawGameItem() {
       <div v-if="data.showProgressContent && data.progressContent" class="absolute top-2 left-2 right-2 bottom-2 dark:bg-gray-700 dark:text-gray-400">
         <div class="p-4 w-full h-full overflow-auto">
           <pre v-html="data.progressContent"></pre>
+        </div>
+      </div>
+
+      <div v-if="isProcessingAction(processingActions.initGameDeletion)" class="absolute top-2 left-2 right-2 bottom-2 dark:bg-gray-700 dark:text-gray-400 opacity-98">
+        <div class="p-4 w-full h-full overflow-auto">
+          <div class="p-2 mb-2">
+            <p>Are you sure you want to delete this game from your local library?</p>
+            <p>No worries, it can re-downloaded again</p>
+          </div>
+
+          <div v-if="data.gameItem.launcher === 'PORT_PROTON'" class="grid w-full md:grid-cols-2">
+            <input id="delete-prefix" v-model="data.deletePrefix" type="checkbox" class="hidden peer" />
+            <label
+              for="delete-prefix"
+              class="flex flex-col items-start justify-between w-full p-5 text-gray-500 border-4 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-600 dark:peer-checked:border-yellow-600 dark:peer-checked:text-gray-300 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-600"
+            >
+              <div class="flex">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-7 stroke-red-500 mr-2">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                  />
+                </svg>
+
+                <span class="w-full text-lg font-semibold">Delete Prefix?</span>
+              </div>
+
+              <div class="w-full text-sm text-wrap break-all mt-3">{{ data.gameItem.realLocalPrefixPath }}</div>
+            </label>
+          </div>
+
+          <div class="grid w-full md:grid-cols-2 mt-4">
+            <div class="grid w-full md:grid-cols-2 gap-12">
+              <button
+                type="button"
+                class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br dark:focus:ring-blue-800 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2"
+                @click="onActionConfirmGameDeletion"
+              >
+                Delete
+              </button>
+
+              <button
+                type="button"
+                class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br dark:focus:ring-red-800 shadow-lg dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2"
+                @click="onActionCancelGameDeletion"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
         </div>
       </div>
 
